a1f73c4b40ce6346e2586d7ea5403f93750248f3
AndreSynZ <45897999+AndreSynZ@users.noreply.github.com>
2018-12-24 21:48:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ client.on('ready', () => {
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
         client.user.setActivity(activities_list[index], { type: "STREAMING", url: "https://www.twitch.tv/somethingluulop"}); // sets bot's activities to one of the phrases in the arraylist.
-    }, 7000); // Runs this every 10 seconds.
+    }, 6000); // Runs this every 10 seconds.
 });
 
 
