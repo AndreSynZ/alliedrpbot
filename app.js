9af12eb48359d0bbaadaf760582aaa0705e8b5ac
ATTSynZ <synz1234567@gmail.com>
2018-12-24 22:37:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -137,12 +137,6 @@ if(command === "play") {
 
 
 
-  if(command === "ping") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const m = await message.channel.send("Ping?")
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
 
   if(command === "say") {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use.
