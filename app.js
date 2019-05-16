90bdf006b283940e6c25041f3fd2d28a4ae299db
AndreSynZ <45897999+AndreSynZ@users.noreply.github.com>
2019-05-16 22:39:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -74,7 +74,18 @@ client.on("message", async message => {
 	
 	
 client.on("guildMemberAdd", function(member) {
-    member.guild.channels.find(x => x.name === "welcome").send(member.toString() + " welcome to the clan.");
+	let embed = new Discord.RichEmbed()
+	.setTitle(member.toString() + "Welcome to Freedom Fighters!")
+	.setDescription("We are the Faction that fights for Freedom!")
+	.addField("Joining the Faction", "You join the Faction by answer the questions at #application-template and pasting the application in #type-your-app-here. It will then show up in #applications")
+	.addField("Missions", "In order to view all of the missions that we are going to do in the future, go to #missions")
+	.addField("Ranks", "All of the ranks are visible at #ranking-structure")
+	.addField("Medals", "The medals you can achieve as rewards are displayed in #medals")
+	.setFooter("Freedom Fighters Database")
+ 	.setThumbnail("https://cdn.discordapp.com/attachments/576545355978637315/576549475200270366/ff.png")
+    	.setTimestamp()	
+	
+    member.guild.channels.find(x => x.name === "welcome").send(embed);
 });
 	
 	
